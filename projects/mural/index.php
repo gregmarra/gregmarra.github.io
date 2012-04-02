9f64da252347b3c94fd69ae91a2ef554d52ed332
Greg Marra <greg.marra@gmail.com>
2012-04-02 00:43:20 -0700

    Initial site commit.

---
+++ projects/mural/index.php
@@ -0,0 +1,63 @@
+<html>
+<head>
+    <title>Mural for Android</title>
+    <link rel="shortcut icon" href="favicon.ico">
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/jquery.lightbox-0.5.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/jquery.lightbox-0.5.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <script type="text/javascript">
+        $(function() {
+            $('#screenshots a').lightBox();
+        });
+    </script>
+</head>
+<body>
+<div id="wrapper">
+    <div id="header">
+        <div id="titles">
+            <h1>Mural for Android</h1>
+            <h2>Social Wallpaper for Android and Facebook</h2>
+        </div>
+    </div>
+    <div id="content">
+        <h2>About Mural</h2>
+        <p><img src="images/muralstack.png" title="Mural connects Android to Facebook to share your wallpapers" /></p>
+        <p>Your phone's wallpaper is your personal customization that shows off who you are and what you care about. Mural lets you share that personalization with all of your friends on Facebook (including the 99% without Android phones) and lets you send and receive wallpaper suggestions with your other friends who have Mural. Show off your cool wallpaper, and recommend cooler ones to your friends.</p>
+        <h2>Get Mural</h2>
+        <p><strike>Mural is available for download on the Android Market. Search "mural" to find it!</strike></p>
+        <p>We accidentally lost control of the getmural.com domain name, and our Facebook auth system was never that great. We have let Mural fade away into app heaven to focus on other projects. Farewell, Mural!</p>
+        <h2>Screenshots</h2>
+        <p id="screenshots">
+            <a href="images/screenshots/feed.png"><img src="images/screenshots/feed_thumb.png" width="160" height="240" /></a>
+            <a href="images/screenshots/uploadtofacebook.png"><img src="images/screenshots/uploadtofacebook_thumb.png"  width="160" height="240" /></a>
+            <a href="images/screenshots/friends.png"><img src="images/screenshots/friends_thumb.png" width="160" height="240" /></a>
+            <br />
+            <a href="images/screenshots/myprofile.png"><img src="images/screenshots/myprofile_thumb.png" width="160" height="240" /></a>
+            <a href="images/screenshots/send_suggestion.png"><img src="images/screenshots/send_suggestion_thumb.png" width="160" height="240" /></a>
+            <a href="images/screenshots/suggestion.png"><img src="images/screenshots/suggestion_thumb.png" width="160" height="240" /></a>
+        </p>
+        <h2>Who Are We?</h2>
+        <p>Mural was created by <a href="http://www.erikdkennedy.com" title="Erik Kennedy's website">Erik Kennedy</a>, Dongwoo Kim, Yunseok Jang, and <a href="http://www.grgmrr.com" title="Greg Marra's website">Greg Marra</a>. Mural was created as part of <a href="http:/www.olin.edu" title="Olin College">Olin College</a>'s <a href="http://www.olin.mobi" title="Olin's Developing Mobile Phone Apps course">Developing Mobile Phone Apps course</a>. Other apps created for the course include <a href="http://www.craigslistener.net">Craigslistener</a>, What's For Dinner, and Flashcards.</p>
+    </div>
+    <div id="nav">
+        <hr />
+        <ul>
+            <li><a href="mailto:getmural@gmail.com">email us</a> | </li>
+            <li><a href="http://www.twitter.com/getmural">follow us on twitter</a> | </li>
+            <li>&copy; 2009</li>
+        </ul>
+    </div>
+</div>
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try{
+var pageTracker = _gat._getTracker("UA-7748587-2");
+pageTracker._trackPageview();
+} catch(err) {}
+</script>
+</body>
+</html>
