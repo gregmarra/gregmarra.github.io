39148a284a1c4e92a69fb7a9bfbf31bbde1dcf5d
Greg Marra <greg.marra@gmail.com>
2012-04-02 22:20:53 -0700

    Add maraudersmap and spidergraphs subproject sites to repo

---
+++ maraudersmap/index.php
@@ -0,0 +1,56 @@
+<html>
+<head>
+    <title>Marauder's Map @ Olin College</title>
+    <link rel="shortcut icon" href="favicon.ico">
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/jquery.lightbox-0.5.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/jquery.lightbox-0.5.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <script type="text/javascript">
+        $(function() {
+            $('#screenshots a').lightBox();
+        });
+    </script>
+</head>
+<body>
+<div id="wrapper">
+    <div id="header">
+        <div id="titles">
+            <h1>Marauder's Map @ Olin College</h1>
+            <h2>Viewer for Android</h2>
+        </div>
+    </div>
+    <div id="content">
+        <h2>About Marauder's Map @ Olin College</h2>
+        <p>The Marauder's Map @ Olin is an opt-in utility that allows students at Olin College to find each other more easily. This page is about an Android client for the Marauder's Map. The client allows users to both locate other Map users, as well as update their location on the map using their Android device's built in wifi.</p>
+        <h2>Get Marauder's Map @ Olin College</h2>
+        <p>Marauder's Map @ Olin College is available for download on the Android Market. Search "olin" to find it! You can also use this barcode to download it.</p>
+        <p><img src="images/market_qr.png" alt="Scan this barcode to get Marauder's Map @ Olin College"/></p>
+        <h2>Screenshots</h2>
+        <p id="screenshots">
+            <a href="images/screenshots/loading.png"><img src="images/screenshots/loading_thumb.png" width="160" height="240" /></a>
+            <a href="images/screenshots/listing.png"><img src="images/screenshots/listing_thumb.png"  width="160" height="240" /></a>
+            <a href="images/screenshots/filtering.png"><img src="images/screenshots/filtering_thumb.png"  width="160" height="240" /></a>
+            <a href="images/screenshots/where_are_you.png"><img src="images/screenshots/where_are_you_thumb.png"  width="160" height="240" /></a>
+        </p>
+        <h2>Who Made This?</h2>
+        <p>Marauder's Map @ Olin College was created by Andy Barry and Ben Fisher. Marauder's Map @ Olin College for Android was created by <a href="http://www.grgmrr.com" title="Greg Marra's website">Greg Marra</a>. Marauder's Map @ Olin College only works at <a href="http://www.olin.edu">Olin College</a>.</p>
+    </div>
+    <div id="nav">
+        <hr />
+        <ul>
+            <li>&copy; 2009</li>
+        </ul>
+    </div>
+</div>
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-7748587-4");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+</body>
+</html>
